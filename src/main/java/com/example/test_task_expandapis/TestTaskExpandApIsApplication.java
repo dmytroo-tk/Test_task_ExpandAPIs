4c2fcdf9b88107fb104f754ee669cb735ca4018b
Dmytro Tkachuk <dmytrooo3.tk@gmail.com>
2023-12-24 15:08:10 +0200

    Init commit

---
+++ src/main/java/com/example/test_task_expandapis/TestTaskExpandApIsApplication.java
@@ -0,0 +1,13 @@
+package com.example.test_task_expandapis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestTaskExpandApIsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestTaskExpandApIsApplication.class, args);
+    }
+
+}
